9a0b4b5910e89239443785100549711caac8f4be
chan_nie <126068623+chennielee@users.noreply.github.com>
2024-03-02 16:00:18 +0900

    mod: deploy

---
+++ src/main/java/com/example/appscheck/AppscheckApplication.java
@@ -0,0 +1,13 @@
+package com.example.appscheck;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class AppscheckApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(AppscheckApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/example/appscheck/AppscheckApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.appscheck;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class AppscheckApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
