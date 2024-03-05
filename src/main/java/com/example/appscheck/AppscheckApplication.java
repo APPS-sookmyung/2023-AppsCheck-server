55cd39c739b768a3847c0874bd79f3f6597149ad
chan_nie <126068623+chennielee@users.noreply.github.com>
2024-03-05 14:22:34 +0900

    add annotation

--- src/main/java/com/example/appscheck/AppscheckApplication.java
+++ src/main/java/com/example/appscheck/AppscheckApplication.java
@@ -2,7 +2,9 @@ package com.example.appscheck;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.context.annotation.PropertySource;
 
+@PropertySource("classpath:security.properties")
 @SpringBootApplication
 public class AppscheckApplication {
 
